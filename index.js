6eefb2b7e0ef574e2d95e080d09579056a8d87ce
koppalaMeghana <koppalameghana25@gmail.com>
2026-06-16 16:16:32 +0530

    Change button selector to '.game-btn'

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const buttons = document.querySelectorAll("button");
+const buttons = document.querySelectorAll(".game-btn");
 const resultEl = document.getElementById("result");
 const resetBtn = document.getElementById("reset-btn");
 
